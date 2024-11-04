19f7225b5b2f3c4df8953147f432d8ac535cf468
Troyboiiiii <Bobpiltover@gmail.com>
2024-11-04 20:06:18 +0200

    Initial project setup with .gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RSSFeedReader.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
